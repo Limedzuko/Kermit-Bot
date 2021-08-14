66781865787fb49dd1e7a5e891c7de0f4505eee1
Limedzuko <87378325+Limedzuko@users.noreply.github.com>
2021-08-14 16:01:35 +0530

    Kick Cog Added |

---
+++ code/commands/kick.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import discord
+
+
+class kick(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+
+ 
+  @commands.command()
+  async def kick(self, ctx, member: discord.Member, *, reason=None):
+  
+
+    ea = discord.Embed(color=discord.Color.red())
+    ea.add_field(name=f"{member} got kicked!", value = f"Reason: **{reason}**")
+  
+    await member.kick(reason=reason)
+    await ctx.send(embed=ea)
+
+    
+  
+
+  
+
+
+
+def setup(bot):
+  bot.add_cog(kick(bot))
